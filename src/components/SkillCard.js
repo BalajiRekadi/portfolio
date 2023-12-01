9ffe108a5206edf2b5386aef0844e0cd86ad16f5
Balaji Rekadi <64888503+BalajiRekadi@users.noreply.github.com>
2023-12-01 12:17:11 +0530

    modified skill cards

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,4 +1,12 @@
-import './skillcard.css';
+import js from '../../public/js.svg';
+import css from '../../public/css.svg';
+import html from '../../public/html.svg';
+import node from '../../public/node.svg';
+import reactjs from '../../public/reactjs.svg';
+import reactquery from '../../public/reactquery.svg';
+import redux from '../../public/redux.svg';
+import typescript from '../../public/typescript.svg';
+import webpack from '../../public/webpack.svg';
 class SkillCard extends HTMLElement {
     constructor() {
       super();
@@ -6,12 +14,24 @@ class SkillCard extends HTMLElement {
   
     connectedCallback() {
       const name = this.getAttribute('name');
-      const src = this.getAttribute('src');
+      let src = '';
+      switch(name) {
+        case 'javascript': src = js; break;
+        case 'HTML': src = html; break;
+        case 'CSS': src = css; break;
+        case 'ReactJS': src = reactjs; break;
+        case 'Redux': src = redux; break;
+        case 'React Query': src = reactquery; break;
+        case 'Typescript': src = typescript; break;
+        case 'Webpack': src = webpack; break;
+        case 'NodeJS': src = node; break;
+        default: break;
+      }
       const size = this.getAttribute('size');
 
       this.innerHTML = `
         <div class="top">
-            <img class="skill-image" width="${size}%"/>
+            <img src="${src}" width="${size}%"/>
         </div>
         <div class="bottom">${name}</div>
       `;
